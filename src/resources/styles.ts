c62c35d0906c40b0f591d61d8f2b9d4437df81f2
Giulio Marescotti <giulio.marescotti@fastwebnet.it>
2023-06-07 20:39:02 +0200

    fix: header restored background

--- src/resources/styles.ts
+++ src/resources/styles.ts
@@ -307,6 +307,11 @@ export const haStyle = css`
   .layout.justified {
     justify-content: space-between;
   }
+  :host {
+    --app-header-background-color: var(--sidebar-background-color);
+    --app-header-text-color: var(--sidebar-text-color);
+    --app-header-border-bottom: 1px solid var(--divider-color);
+  }
 `;
 
 export const haStyleDialog = css`
